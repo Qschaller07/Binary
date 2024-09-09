4fa92dfa25dc78676421fca033ed2451abacf641
Quin Schaller <qaschaller@gmail.com>
2024-09-09 09:40:59 -0500

    Initial Commit

---
+++ Binary/BinaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BinaryApp.swift
+//  Binary
+//
+//  Created by Quin Schaller on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct BinaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
