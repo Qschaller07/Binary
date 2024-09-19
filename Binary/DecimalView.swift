505fb88766172b7d2925553dfc7b6be4b35cb64c
Jonas <jonas.gregory@outlook.com>
2024-09-19 09:11:39 -0500

    u

--- Binary/DecimalView.swift
+++ Binary/DecimalView.swift
@@ -2,14 +2,39 @@
 //  DecimalView.swift
 //  Binary
 //
-//  Created by Jonas R. Gregory on 9/19/24.
+//  Created by Quin Schaller on 9/19/24.
 //
 
 import SwiftUI
 
 struct DecimalView: View {
+    @State var PutInDecimal: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Change Decimal to Binary or Hexa!")
+                .font(.largeTitle)
+                .multilineTextAlignment(.center)
+                .padding()
+            Divider()
+            Spacer()
+            Text("Enter Decimal:")
+                .multilineTextAlignment(.center)
+                .font(.largeTitle)
+            TextField("Enter Decimal; Any Number!", text: $PutInDecimal)
+                .multilineTextAlignment(.center)
+                .frame(width: 300)
+                .textFieldStyle(.roundedBorder)
+                .font(.title2)
+            Text("Decmial to Binary: \n \(PutInDecimal.decimalToBinary)")
+                .padding()
+                .font(.title)
+                .multilineTextAlignment(.center)
+            Text("Decimal to Hexa: \n \(PutInDecimal.decimalToHexa)")
+                .padding()
+                .font(.title)
+                .multilineTextAlignment(.center)
+            Spacer()
+        }
     }
 }
 
