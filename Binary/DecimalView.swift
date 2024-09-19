35b269e204b33d694e7dd9e4bf37ac773aac6499
Jonas <jonas.gregory@outlook.com>
2024-09-19 09:11:30 -0500

    f

---
+++ Binary/DecimalView.swift
@@ -0,0 +1,18 @@
+//
+//  DecimalView.swift
+//  Binary
+//
+//  Created by Jonas R. Gregory on 9/19/24.
+//
+
+import SwiftUI
+
+struct DecimalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DecimalView()
+}
